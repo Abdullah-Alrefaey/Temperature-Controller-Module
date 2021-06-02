baf6ed3549de69e1ae4142f97712107516d50c1f
Refaey <abdullah.m.alrefaey@gmail.com>
2021-06-02 20:52:40 +0200

    ADC Driver

---
+++ Temperature-Controller-Module/libs/ADC_driver.h
@@ -0,0 +1,17 @@
+/*
+ * ADC_driver.h
+ *
+ * Created: 3/3/2018 5:00:21 PM
+ *  Author: Mohamed Zaghlol
+ */ 
+
+
+#ifndef ADC_DRIVER_H_
+#define ADC_DRIVER_H_
+
+void ADC_vinit(void);
+unsigned short ADC_u16Read(void);
+
+
+
+#endif /* ADC_DRIVER_H_ */
