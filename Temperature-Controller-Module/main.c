f3632e197493ba0fdaf26f4b2dc7d62740b5c4a6
Refaey <abdullah.m.alrefaey@gmail.com>
2021-06-02 16:31:57 +0200

    Initiate Atmel Project

---
+++ Temperature-Controller-Module/main.c
@@ -0,0 +1,19 @@
+/*
+ * Temperature-Controller-Module.c
+ *
+ * Created: 02-Jun-21 4:17:47 PM
+ * Author : Refaey
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+    }
+}
+
