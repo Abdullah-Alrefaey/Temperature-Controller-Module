ffe5088d51a80e529473de479f8297d7fa2ab11f
Refaey <abdullah.m.alrefaey@gmail.com>
2021-06-03 14:26:51 +0200

    Added Display Manager

---
+++ Temperature-Controller-Module/Managers/DSP_Manager.h
@@ -0,0 +1,29 @@
+/*
+ * DSP_Manager.h
+ *
+ * Created: 03-Jun-21 2:09:46 PM
+ *  Author: Refaey
+ */ 
+
+
+#ifndef DSP_MANAGER_H_
+#define DSP_MANAGER_H_
+
+#define F_CPU 16000000UL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include "../Drivers/LCD.h"
+
+void WelcomeScreen(void);
+void IdleScreen(void);
+void Update_SET_Temperature(int value);
+void Update_CRT_Temperature(int value);
+void Update_STATE(char* state);
+
+#endif /* DSP_MANAGER_H_ */
