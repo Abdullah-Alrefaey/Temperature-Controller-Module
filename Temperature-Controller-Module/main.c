75f01badfceb9272c21fd1c2670a5cacec6c33e9
Refaey <abdullah.m.alrefaey@gmail.com>
2021-06-03 14:52:14 +0200

    Edit Main

--- Temperature-Controller-Module/main.c
+++ Temperature-Controller-Module/main.c
@@ -6,26 +6,19 @@
  */ 
 
 #define F_CPU 16000000UL
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include <avr/io.h>
 #include <util/delay.h>
 
-#include "Drivers/LCD.h"
 #include "Managers/DSP_Manager.h"
 
 int main(void)
 {
 	// Start Main Application
-	/* Setup LCD PORT and PINs Configurations */
-	LCD_vInit();
-	// WelcomeScreen();
-	
+	WelcomeScreen();
 	IdleScreen();
-	
-    while (1) 
+
+    while (1)
     {
 		
     }
