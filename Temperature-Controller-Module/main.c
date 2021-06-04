87553e7061246b1b650047067fcad48e10d07634
Salo7a <salo7a98x@gmail.com>
2021-06-04 19:17:43 +0200

    PWM Pin Change

--- Temperature-Controller-Module/main.c
+++ Temperature-Controller-Module/main.c
@@ -11,12 +11,8 @@ int main(void)
 	// Start Main Application
 	//WelcomeScreen();
 	//IdleScreen();
-	PWM_Init(0,1,1);
 	PWM_Init(2,1,2);
-	DIO_vsetPINDir('D', 6, 1);
-	DIO_Write_PIN('D', 6, 1);
-	uint8_t duty = 200;
-	Set_PWM_Duty(0, duty);
+	uint8_t duty = 127;
 	Set_PWM_Duty(2, duty);
     while (1)
     {
