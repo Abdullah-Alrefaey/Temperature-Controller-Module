1857f70f00760ca4e6904354e3e7da38c5c74ceb
Refaey <abdullah.m.alrefaey@gmail.com>
2021-06-05 15:32:30 +0200

    Added TC72 Driver v1

---
+++ Temperature-Controller-Module/Drivers/TC72.c
@@ -0,0 +1,14 @@
+/*
+ * TC72.c
+ *
+ * Created: 05-Jun-21 2:34:36 PM
+ *  Author: Refaey
+ */ 
+
+#include "TC72.h"
+
+int tc72_read()
+{
+	int value = 30;
+	return value;
+}
---
+++ Temperature-Controller-Module/Drivers/TC72.h
@@ -0,0 +1,16 @@
+/*
+ * TC72.h
+ *
+ * Created: 05-Jun-21 2:34:47 PM
+ *  Author: Refaey
+ */ 
+
+
+#ifndef TC72_H_
+#define TC72_H_
+
+#include "DIO.h"
+
+int tc72_read();
+
+#endif /* TC72_H_ */
