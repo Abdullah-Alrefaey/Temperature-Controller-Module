957acc0a4ee8503f1f399e9cc50973b76eeb5256
Refaey <abdullah.m.alrefaey@gmail.com>
2021-06-05 19:18:44 +0200

    Edit .gitignore and Refactoring

--- Temperature-Controller-Module/main.c
+++ Temperature-Controller-Module/main.c
@@ -5,20 +5,15 @@
  
 #define F_CPU 16000000UL
 
-#include "Managers/DSP_Manager.h"
+#include "Managers/Schedular.h"
 #include "Managers/TMP_Manager.h"
+#include "Managers/DSP_Manager.h"
 
 #include "Drivers/PWM.h"
 #include "Drivers/keypad_driver.h"
 #include "Drivers/Timer.h"
 
-#include <avr/io.h>
 #include <util/delay.h>
-#include <avr/interrupt.h>
-
-volatile uint8_t counter1 = 0;
-volatile uint8_t counter2 = 0;
-volatile uint8_t counter3 = 0;
 
 extern uint8_t CRT_Temperature;
 
@@ -47,21 +42,6 @@ int main(void)
 		
 		Display_CRT_Temperature(CRT_Temperature);
 		
-		// pressed_key = keypad_u8check_press();
 		
-		/*op_pressed = check_OPKey();
-		if (op_pressed == 1)
-		{
-			LCD_vSend_string("ON!");
-			_delay_ms(1);
-		}*/
     }
-}
-
-
-ISR(TIMER0_COMP_vect)
-{
-	counter1++;
-	counter2++;
-	counter3++;
 }
