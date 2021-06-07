65b2d16c82ca4cd7ee02595fb4a6c5fa50b2f802
Salo7a <salo7a98x@gmail.com>
2021-06-07 23:21:24 +0200

    Update Main

--- Temperature-Controller-Module/main.c
+++ Temperature-Controller-Module/main.c
@@ -4,7 +4,7 @@
  */ 
  
 /*#define F_CPU 8000000UL*/
-#define F_CPU 16000000UL
+
 #include "Managers/Schedular.h"
 #include "Managers/TMP_Manager.h"
 #include "Managers/DSP_Manager.h"
@@ -28,13 +28,13 @@ int main(void)
 	WelcomeScreen();
 	IdleScreen();
 	keypad_vInit();
-	TC72_Init(One_Shot);
+	TC72_Init(ONE_SHOT_MODE);
 	Schedular_vInit();
 	LEDs_States_vInit();
 		
 	while (1)
     {
-		// Display_SET_Temperature(SET_Temperature);
+		Display_SET_Temperature(SET_Temperature);
 		Display_CRT_Temperature(CRT_Temperature);
 		Display_STATE(states[state_indx]);
 		
@@ -42,53 +42,54 @@ int main(void)
 		 * Update SET_Temperature Value From The User
 		 * Check if User Pressed '#' To Switch to OPERATION STATE
 		 */
-		if (state_indx == 0)
+		if (state_indx == STANDBY_INDEX)
 		{
 			/* PWM OFF */
 			Heater_vDisable();
 			
 			Update_SET_Temperature();
-			// Display_SET_Temperature(SET_Temperature);
-			// Check_OPERATION_State_Key();
+			Display_SET_Temperature(SET_Temperature);
+			Check_HASH_Key();
 		}
 		
 		/* Check if you are in OPERATION STATE */
-		if (state_indx == 1)
+		if (state_indx == OPERATION_INDEX)
 		{
 			/* Start Heater */
 			Heater_vInit();
-			
-			/* Update PWM Wave */
-			SetHeaterVolt(Vt, Vr);
 			
 			Update_CRT_Temperature();
 			Update_Vt();
 			Update_Vr();
 			
-			Check_STANDBY_State();
+			/* Update PWM Wave */
+			Heater_vSet_Volt(Vt, Vr);
+			
+			Check_HASH_Key();
 			Check_NORMAL_State();
+			Check_ERROR_State();
 			
 			/* This function should handle Error 3 min */
 			Check_ERROR_State_Timer();
 		}
 		
 		/* Check if you are in NORMAL STATE */
-		if (state_indx == 2)
+		if (state_indx == NORMAL_INDEX)
 		{
 			/* PWM OFF */
 			Heater_vDisable();
 			
 			Update_CRT_Temperature();
 						
-			Check_STANDBY_State();			
+			Check_HASH_Key();		
 			Check_OPERATION_State();	
 			Check_ERROR_State();		
 		}
 		
 		/* Check if you are in ERROR STATE */
-		if (state_indx == 3)
+		if (state_indx == ERROR_INDEX)
 		{
-			/* PWM OFF */
+			/* PWM and ADC OFF */
 			Heater_vDisable();
 			
 			/* Must POWER OFF */
