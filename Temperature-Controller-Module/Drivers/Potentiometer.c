bace07be6d70dbf594921437733c06a1f2d7d1e0
ahmad <ahmedmageed19@gmail.com>
2021-06-08 01:02:24 +0200

    Driver Format

--- Temperature-Controller-Module/Drivers/Potentiometer.c
+++ Temperature-Controller-Module/Drivers/Potentiometer.c
@@ -1,27 +1,38 @@
-/*
- * Potentiometer.c
- *
- * Created: 05-Jun-21 4:23:23 PM
- *  Author: Refaey
- */ 
-
+/************************************************************************/
+/*                          Potentiometer Driver                        */
+/************************************************************************/
 #include "Potentiometer.h"
 #include "ADC.h"
 
+/************************************************************************/
+/* Function Description:                                                */
+/* Potentiometer Initiation depends on the ADC Driver Initiation,       */
+/* SEE DOCUMENTATION IN ADC.H                                           */
+/************************************************************************/
 void Potentiometer_vInit(void)
 {
 	ADC_vinit();	
 }
 
+/************************************************************************/
+/* Function Description:                                                */
+/* Potentiometer Disable Functionality                                  */
+/* SEE DOCUMENTATION IN ADC.H                                           */
+/************************************************************************/
 void Potentiometer_vDisable(void)
 {
 	ADC_vDisable();
 }
 
+/************************************************************************/
+/* Function Description:                                                */
+/* Read Voltage Applied on the Potentiometer using the ADC Functionality*/
+/* and Map the Output to 5 Volts                                        */
+/************************************************************************/
 double Potentiometer_Read(void)
 {
 	double VPot = 0;
-	uint16_t ADCReading = 0;
+	uint16_t ADCReading = 0U;
 	ADCReading = ADC_u16Read();
 	VPot = (5.0/1023.0)*ADCReading;
 	return VPot;
