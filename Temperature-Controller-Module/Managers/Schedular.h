b0f83301ba0108150e732d82a8a8482f407744b0
ahmad <ahmedmageed19@gmail.com>
2021-06-08 00:21:39 +0200

    Scheduler Format

--- Temperature-Controller-Module/Managers/Schedular.h
+++ Temperature-Controller-Module/Managers/Schedular.h
@@ -1,20 +1,26 @@
-/*
- * Schedular.h
- *
- * Created: 05-Jun-21 4:15:06 PM
- *  Author: Refaey
- */ 
-
-
+/************************************************************************/
+/*                          Scheduler Manager                           */
+/************************************************************************/
 #ifndef SCHEDULAR_H_
 #define SCHEDULAR_H_
 
+/************************************************************************/
+/* Manager Imports                                                      */
+/************************************************************************/
 #include "../Drivers/Timer.h"
 #include "Mode_Config.h"
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-void Schedular_vInit();
+/************************************************************************/
+/* Driver Functions                                                     */
+/************************************************************************/
+
+/************************************************************************/
+/* Function Description : Timer Functionality Initiator                 */
+/* Function Arguments   : void                                          */
+/* Function Returns     : void                                          */
+/************************************************************************/
+void Schedular_vInit(void);
 
 #endif /* SCHEDULAR_H_ */
