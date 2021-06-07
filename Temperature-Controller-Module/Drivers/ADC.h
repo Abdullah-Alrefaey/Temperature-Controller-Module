a7ee73106a7772f4c79d3bfefdb94b5e52fe1b8b
Salo7a <salo7a98x@gmail.com>
2021-06-07 23:24:51 +0200

    ADC Header Updates

--- Temperature-Controller-Module/Drivers/ADC.h
+++ Temperature-Controller-Module/Drivers/ADC.h
@@ -1,17 +1,40 @@
-/*
- * ADC_driver.h
- *
- */ 
+/************************************************************************/
+/*                          ADC Driver                                  */
+/************************************************************************/
 
 
 #ifndef ADC_DRIVER_H_
 #define ADC_DRIVER_H_
-
+/************************************************************************/
+/*                          Library Imports                             */
+/************************************************************************/
 #include <avr/io.h>
+/************************************************************************/
+/*                          Driver Functions                            */
+/************************************************************************/
 
+/*
+	Function Name        : ADC_vInit
+	Function Returns     : void
+	Function Arguments   : void
+	Function Description : Initializes ADC on PA7 With 1/128 Prescaler
+*/
+void ADC_vInit(void);
 
-void ADC_vinit(void);
+/*
+	Function Name        : ADC_vInit
+	Function Returns     : void
+	Function Arguments   : void
+	Function Description : Initializes ADC on PA7 With 1/128 Prescaler
+*/
 uint16_t ADC_u16Read(void);
+
+/*
+	Function Name        : ADC_vDisable
+	Function Returns     : void
+	Function Arguments   : void
+	Function Description : Disables ADC
+*/
 void ADC_vDisable(void);
 
 #endif /* ADC_DRIVER_H_ */
