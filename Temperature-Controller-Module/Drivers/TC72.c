ab1f7aeb753795b672e84cdbb0219fac18d0d1f5
ahmad <ahmedmageed19@gmail.com>
2021-06-07 20:53:09 +0200

    TC72 Driver Format, Delay Violation Removal

--- Temperature-Controller-Module/Drivers/TC72.c
+++ Temperature-Controller-Module/Drivers/TC72.c
@@ -1,15 +1,19 @@
-/*
- * TC72.c
- *
- * Created: 05-Jun-21 2:34:36 PM
- * Author: Ahmad Abdalmageed
- */ 
-
+/************************************************************************/
+/*                          Keypad Driver                               */
+/*                   Author: Ahmad Abdalmageed                          */
+/************************************************************************/
 #include "TC72.h"
 
-
+/************************************************************************/
+/* Function Description:                                                */
+/* TC72 SPI Communication Protocol Initiator Function, Initiates the SPI*/
+/* Master Mode and Select the Control Register with 0x80 Address and    */
+/* Specify the Sensor's Operation Mode. One-Shot, Only one Temperature  */
+/* is taken on writing to the Sensor. Continous Mode, the sensor takes  */
+/* a reading ever 150 ms and store it in the Data Register              */ 
+/************************************************************************/
 void TC72_Init(char mode){
-	
+	// Initiate SPI Master in SPI 
 	SPI_MasterInit();
 	
 	// Create a Rising Edge on the	
@@ -27,6 +31,14 @@ void TC72_Init(char mode){
 	DIO_Write_PIN('B', 4, 0);
 }
 
+/************************************************************************/
+/* Function Description:                                                */
+/* Main TC72 Reading Operation Function, TC72 Write mode is done with CE*/
+/* Chip Select being HIGH, Then the Reading Process starts, The Master  */
+/* Selects from MSB (Lower Resolution Temp.) or LSB (Higher Resolution  */
+/* Temp.). We are only Interested in the MSB which is selected from the */
+/* 0x02 Register.                                                       */
+/************************************************************************/
 char TC72_Read(){
 	// Create a Rising Edge on the
 	// TC72 Port to Start SPI Communication 
@@ -37,14 +49,13 @@ char TC72_Read(){
 	// Temperature Measurement
 	char temp;
 	temp = SPI_MasterTransmitchar(0x02);
-	_delay_ms(1);
+	//_delay_us(20); // Removed Violation for Testing 
 	
 	// Signal Check with the TC72, TODO: Check Necessity of This Step
 	temp = SPI_MasterTransmitchar(0x00);
-	_delay_ms(1);
+	//_delay_us(20); // Removed Violation for Testing 
 	
 	// Finish Communication
 	DIO_Write_PIN('B', 4, 0);
-	
 	return temp;
 }
