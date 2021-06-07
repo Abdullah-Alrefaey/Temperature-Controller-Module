fbba1266d7a8499074cf43a31a614ed5b1c36555
Salo7a <salo7a98x@gmail.com>
2021-06-08 01:00:05 +0200

    Potentiometer.c MISRA Updates

--- Temperature-Controller-Module/Drivers/Potentiometer.c
+++ Temperature-Controller-Module/Drivers/Potentiometer.c
@@ -17,7 +17,7 @@ void Potentiometer_vDisable(void)
 double Potentiometer_Read(void)
 {
 	double VPot = 0;
-	uint16_t ADCReading = 0;
+	uint16_t ADCReading = 0U;
 	ADCReading = ADC_u16Read();
 	VPot = (5.0/1023.0)*ADCReading;
 	return VPot;
