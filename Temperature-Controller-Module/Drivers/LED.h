b7be5941b67f3eb988b64876fa4ed3f38b93e2e2
ahmad <ahmedmageed19@gmail.com>
2021-06-07 23:12:32 +0200

    LED Driver Format

--- Temperature-Controller-Module/Drivers/LED.h
+++ Temperature-Controller-Module/Drivers/LED.h
@@ -1,55 +1,39 @@
-/*
- * LED.h
- *
- * Created: 07-Sep-19 4:56:29 PM
- *  Author: Abdullah Mohammed
- */ 
-
-
+/************************************************************************/
+/*                          LED    Driver                               */
+/************************************************************************/
 #ifndef LED_H_
 #define LED_H_
-
+ 
+/************************************************************************/
+/* Driver  Imports                                                      */
+/************************************************************************/
 #include <stdint.h>
 
-/*
-	Function Name        : LED_vInit
-	Function Returns     : void
-	Function Arguments   : char portName, uint8_t pinNumber
-	Function Description : Initialize the pin as an output pin to connect the led.
-*/
+/************************************************************************/
+/* Driver Functions                                                     */
+/************************************************************************/
+
+/************************************************************************/
+/* Function Description : Initialize the pin as an output pin to connect*/
+/*						  the led                                       */
+/* Function Arguments   : char portName, uint8_t pinNumber              */
+/* Function Returns     : void                                          */
+/************************************************************************/
 void LED_vInit(char portName, uint8_t pinNumber);
 
-/*
-	Function Name        : LED_vTurnOn
-	Function Returns     : void
-	Function Arguments   : char portName, uint8_t pinNumber
-	Function Description : Turn on the led connected to the given pin and port.
-*/
+/************************************************************************/
+/* Function Description : Turn on the led connected to the given pin    */
+/*						  and port                                      */
+/* Function Arguments   : char portName, uint8_t pinNumber              */
+/* Function Returns     : void                                          */
+/************************************************************************/
 void LED_vTurnOn(char portName, uint8_t pinNumber);
 
-/*
-	Function Name        : LED_vTurnOff
-	Function Returns     : void
-	Function Arguments   : char portName, uint8_t pinNumber
-	Function Description : Turn off the led connected to the given pin and port.
-*/
+/************************************************************************/
+/* Function Description : Turn OFF the led connected to the given pin   */
+/*						  and port                                      */
+/* Function Arguments   : char portName, uint8_t pinNumber              */
+/* Function Returns     : void                                          */
+/************************************************************************/
 void LED_vTurnOff(char portName, uint8_t pinNumber);
-
-/*
-	Function Name        : LED_vToggle
-	Function Returns     : void
-	Function Arguments   : char portName, uint8_t pinNumber
-	Function Description : Toggle the led connected to the given pin and port.
-*/
-void LED_vToggle(char portName, uint8_t pinNumber);
-
-
-/*
-	Function Name        : LED_u8ReadStatus
-	Function Returns     : unsigned char
-	Function Arguments   : char portName, uint8_t pinNumber
-	Function Description : Returns 0 if the led connected to the given port and pin is off or return 1 if it was on.
-*/
-unsigned char LED_u8ReadStatus(char portName, uint8_t pinNumber);
-
 #endif /* LED_H_ */
