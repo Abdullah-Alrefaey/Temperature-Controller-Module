e65d43aff376b2d92b1ef29aabf60da6d47bbec9
ahmad <ahmedmageed19@gmail.com>
2021-06-08 00:53:53 +0200

    Managers Formats

--- Temperature-Controller-Module/Managers/DSP_Manager.h
+++ Temperature-Controller-Module/Managers/DSP_Manager.h
@@ -1,22 +1,59 @@
-/*
- * DSP_Manager.h
- *
- * Created: 03-Jun-21 2:09:46 PM
- *  Author: Refaey
- */ 
-
-
+/************************************************************************/
+/*                          Display Manager                             */
+/************************************************************************/
 #ifndef DSP_MANAGER_H_
 #define DSP_MANAGER_H_
 
+/************************************************************************/
+/* Manager Imports                                                      */
+/************************************************************************/
 #include "../Drivers/LCD.h"
 #include "Mode_Config.h"
 
+/************************************************************************/
+/* Manager Functions                                                    */
+/************************************************************************/
+
+/************************************************************************/
+/* Function Description : Welcome Screen Display Function               */
+/* Function Arguments   : void                                          */
+/* Function Returns     : void                                          */
+/************************************************************************/
 void WelcomeScreen(void);
+
+/************************************************************************/
+/* Function Description : Idle Screen Display Function                  */
+/* Function Arguments   : void                                          */
+/* Function Returns     : void                                          */
+/************************************************************************/
 void IdleScreen(void);
+
+/************************************************************************/
+/* Function Description : Display Set Temperature Function              */
+/* Function Arguments   : uint8_t value                                 */
+/* Function Returns     : void                                          */
+/************************************************************************/
 void Display_SET_Temperature(uint8_t value);
+
+/************************************************************************/
+/* Function Description : Display Current Temperature Function          */
+/* Function Arguments   : uint8_t value                                 */
+/* Function Returns     : void                                          */
+/************************************************************************/
 void Display_CRT_Temperature(uint8_t value);
+
+/************************************************************************/
+/* Function Description : Display Error Form in Current Temperature     */
+/* Function Arguments   : uint8_t value                                 */
+/* Function Returns     : void                                          */
+/************************************************************************/
 void Display_CRT_Temperature_ERROR();
+
+/************************************************************************/
+/* Function Description : Display Current Program State                 */
+/* Function Arguments   : char* state                                   */
+/* Function Returns     : void                                          */
+/************************************************************************/
 void Display_STATE(char* state);
 
 #endif /* DSP_MANAGER_H_ */
