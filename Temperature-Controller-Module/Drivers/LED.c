d7788fc8234a52becefc9f73f08a3f9635718bfa
Salo7a <salo7a98x@gmail.com>
2021-06-10 01:13:52 +0200

    Solve LED Driver MISRA Warnings

--- Temperature-Controller-Module/Drivers/LED.c
+++ Temperature-Controller-Module/Drivers/LED.c
@@ -8,15 +8,15 @@
 
 void LED_vInit(char portName, uint8_t pinNumber)
 {
-	// Set the given pin in the given port as an output
-	// Write 1 on the pin
-	DIO_vsetPINDir(portName, pinNumber, 1);
+	/* Set the given pin in the given port as an output
+	   Write 1 on the pin */
+	DIO_vsetPINDir(portName, pinNumber, 1U);
 }
 
 void LED_vTurnOn(char portName, uint8_t pinNumber)
 {
-	// Set the given pin in the given port to one (on)
-	DIO_Write_PIN(portName, pinNumber, 1);
+	/* Set the given pin in the given port to one (on) */
+	DIO_Write_PIN(portName, pinNumber, 1U);
 }
 
 void LED_vTurnOff(char portName, uint8_t pinNumber)
