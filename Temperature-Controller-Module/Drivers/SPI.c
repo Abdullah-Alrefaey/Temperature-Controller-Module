def400d8eea909fd79f51ec935ebc619c10b5164
Salo7a <salo7a98x@gmail.com>
2021-06-10 01:33:06 +0200

    Solved SPI Driver MISRA Warnings

--- Temperature-Controller-Module/Drivers/SPI.c
+++ Temperature-Controller-Module/Drivers/SPI.c
@@ -11,26 +11,26 @@ extern char PORTS[4];
 /************************************************************************/
 void SPI_MasterInit(void)
 {	
-	// Set SS / MOSI / SCK  as output pins for master*/
-	DIO_vsetPINDir(PORTS[1], 4, 1);
-	DIO_vsetPINDir(PORTS[1], 5, 1);
-	DIO_vsetPINDir(PORTS[1], 7, 1);
+	/* Set SS / MOSI / SCK  as output pins for master */
+	DIO_vsetPINDir(PORTS[1], 4U, 1U);
+	DIO_vsetPINDir(PORTS[1], 5U, 1U);
+	DIO_vsetPINDir(PORTS[1], 7U, 1U);
 	
-	// Set MISO as Input
-	DIO_vsetPINDir(PORTS[1], 6, 0);
+	/* Set MISO as Input */
+	DIO_vsetPINDir(PORTS[1], 6U, 0U);
 	
-	// Enable Master mode
+	/* Enable Master mode */
 	SPCR |= (U_ONE << MSTR);
 	
-	// Select SPI Mode, Found Mode Compatible with Proteus 8.10
+	/* Select SPI Mode, Found Mode Compatible with Proteus 8.10 */
 	SPCR &= ~(U_ONE << CPOL);
 	SPCR |= (U_ONE << CPHA);
 
-	// Set clock to SC/128
+	/* Set clock to SC/128 */
 	SPCR |= (U_ONE << SPR0);
 	SPCR |= (U_ONE << SPR1);
 	
-	// Enable SPI
+	/* Enable SPI */
     SPCR |= (U_ONE << SPE);
 }
 
@@ -42,14 +42,14 @@ void SPI_MasterInit(void)
 /************************************************************************/
 char SPI_MasterTransmitchar(char Data)
 {	
-	// Write to SPDR 
+	/* Write to SPDR */
 	SPDR = Data;
 	
 	while( ((SPSR & (U_ONE << SPIF)) >> SPIF) == U_ZERO )
 	{
-			// Await SPI Communication to Finish	
+			/* Await SPI Communication to Finish */
 	}
 	
-	// FLush SPDR Register
+	/* FLush SPDR Register */
 	return SPDR ;
 }
