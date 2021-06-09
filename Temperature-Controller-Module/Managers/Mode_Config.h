457af2b93243fdcd81d2caa168e4e25376065f34
Salo7a <salo7a98x@gmail.com>
2021-06-10 01:35:30 +0200

    Solved Mode Configuration MISRA Warnings

--- Temperature-Controller-Module/Managers/Mode_Config.h
+++ Temperature-Controller-Module/Managers/Mode_Config.h
@@ -7,21 +7,20 @@
 /************************************************************************/
 /* Manager Definitions                                                  */
 /************************************************************************/
-#define STANDBY_INDEX 0
-#define OPERATION_INDEX 1
-#define NORMAL_INDEX 2
-#define ERROR_INDEX 3
+#define STANDBY_INDEX 0U
+#define OPERATION_INDEX 1U
+#define NORMAL_INDEX 2U
+#define ERROR_INDEX 3U
 
-#define STANDBY_LED 0
-#define OPERATION_LED 1
-#define NORMAL_LED 2
-#define ERROR_LED 3
+#define STANDBY_LED 0U
+#define OPERATION_LED 1U
+#define NORMAL_LED 2U
+#define ERROR_LED 3U
 
 /************************************************************************/
 /* Manager Imports                                                      */
 /************************************************************************/
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
